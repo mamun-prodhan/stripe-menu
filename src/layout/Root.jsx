5e63b4f275fa641d2cac1c012832b63173e93e1d
mamun-prodhan <mamunpr9@gmail.com>
2024-01-06 08:12:33 +0600

    first commit

---
+++ src/layout/Root.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
+
+const Root = () => {
+  return (
+    <div className="h-screen bg-gradient-to-br from-indigo-500 to-pink-700 via-blue-800 antialiased bg-no-repeat text-white">
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+    </div>
+  );
+};
+
+export default Root;
